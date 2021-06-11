d6897bde6798333175d260e9655c5f1b216fccfa
tmhuyett <toddhuyett@gmail.com>
2021-06-11 16:36:51 -0400

    'd'

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+
+import Amplify from "aws-amplify"
+import config from "../src/aws-exports"
+import Link from "next/link"
+
+Amplify.configure({
+  ...config,
+  ssr: true,
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+<>
+        <nav className="navbar" style={{ display: "flex" }}>
+          <Link href="/">Home</Link>
+          <Link href="/profile">Login</Link>
+        </nav>
+        <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
